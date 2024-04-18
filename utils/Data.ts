8a44270e11a602b183d712109b7f8e39ceb45986
himanxu1 <technophilic000@gmail.com>
2024-04-18 10:55:01 +0530

    data-file-added

---
+++ utils/Data.ts
@@ -0,0 +1,28 @@
+export const data = [
+    {
+        date:"2021-09-01",
+        time:'10:00 PM',
+        reportName:'APP_URL_USAGE_(Exclude_Half_Days).csv',
+    },
+    {
+        date:"2021-09-01",
+        time:'07:00 PM',
+        reportName:'RUR_USAGE_(Exclude_Half_Days).csv',
+    },
+    {
+        date:"2021-09-11",
+        time:'11:00 AM',
+        reportName:'RUR_L2_SAGE_(Exclude_Half_Days).csv',
+    },
+    {
+        date:"2021-08-21",
+        time:'10:00 PM',
+        reportName:'Benchmarking_Report_Comparative_Analysis.csv',
+    },
+    {
+        date:"2021-03-12",
+        time:'10:00 PM',
+        reportName:'Market_Pulse_Report-Q2-2024.csv',
+    },
+
+]
