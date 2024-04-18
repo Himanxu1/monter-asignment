9ab3218873a6c2f4ab19e540eaf6e098294d2c58
himanxu1 <technophilic000@gmail.com>
2024-04-18 11:27:36 +0530

    added-dailog-box

---
+++ app/components/DailogBox.tsx
@@ -0,0 +1,13 @@
+
+const DailogBox  = ({close}) => {
+  return (
+    <div className="fixed inset-0 z-10 overflow-y-auto bg-gray-500 bg-opacity-75 flex justify-center items-center">
+          <div className="bg-white rounded-md w-[90%] h-[600px] p-4">
+            <h2 className="text-2xl mb-4 text-center">Recently Generated Reports</h2>
+           
+          </div>
+        </div>
+  )
+}
+
+export default DailogBox
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,36 @@
+'use client'
+import React, {useState} from 'react';
+import DailogBox from './components/DailogBox';
 
+export default function Home(){
+  const [show, setShow] = useState(false)
+  const handleShowClick = () =>{
+    setShow(!show)
+  }
 
-export default function Home() {
+  const closeDialog = () => {
+    setShow(false);
+  };
+
+ 
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div  className="flex justify-center items-center mt-10 flex-col">
+    <h1 className="text-3xl mb-52">Monter Assignment</h1>
+    <button className="px-4 py-2 border border-black rounded hover:bg-black hover:text-white hover:border-none" 
+    onClick={handleShowClick}
+    >Show</button>
+  {show && (
+        <DailogBox close={closeDialog} />
+        )}
+    </div>
   );
 }
+
+
+
+
+
+
+
+
+
